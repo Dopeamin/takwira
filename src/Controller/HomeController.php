1080f5bd0108bc36374ce7d70cae7a7ad98cf04e
Dopeamine <hamdouni.medamin@gmail.com>
2021-01-15 17:20:30 +0100

    fixed root problem

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
