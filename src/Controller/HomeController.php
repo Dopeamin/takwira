ac63901cf4a8f1f349d721d2c89c6b902c135450
Dopeamine <hamdouni.medamin@gmail.com>
2021-01-15 01:09:07 +0100

    title fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        $page="home";
+        $page="Home";
         return $this->render('home/index.html.twig',[
             'page'=>$page
         ]);
