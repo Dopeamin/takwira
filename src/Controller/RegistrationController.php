42b01169feae9ada39a3a94dc9cd6fa76161da17
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-04-21 16:37:21 +0100

    mailer bugged

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -60,14 +60,14 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            
+            /*
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
                     ->from(new Address('no-reply@takwira.com', 'No Reply'))
                     ->to($user->getUserEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+            );*/
                     $this->addFlash('failure', 'A verification email has been sent to your email');
                     
                     return $this->redirectToRoute('login');
